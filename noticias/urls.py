268ba7b93414a55471d4e3fa33a56f475c84460a
guivargas95 <guisouzavargas@outlook.com>
2021-10-28 11:18:30 -0300

    Modificações para publicação na AWS

--- noticias/urls.py
+++ noticias/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('edita_noticia', views.atualiza_noticia, name='atualiza_noticia'),
     path('edita/<int:noticia_id>', views.edita_noticia, name='edita_noticia'),
     path('buscar', views.buscar, name='buscar'),
+    path('buscar_dashboard', views.buscar_dashboard, name='buscar_dashboard'),
     #path('cria/receita', cria_receita, name='cria_receita'),
     #path('deleta/<int:receita_id>', deleta_receita, name='deleta_receita'),
     #path('edita/<int:receita_id>', edita_receita, name='edita_receita'),
